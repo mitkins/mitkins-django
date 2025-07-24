9b1c2d7db2b0cb74d1aefc2d2c8b1c6be43de0ac
Hamish Murphy <hamishmurphy@gmail.com>
2025-07-24 17:31:09 +1000

    Letter and resume

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("lookahead/3916206", views.qsic_resume, name="qsic_resume"),
+    path("lookahead/3916206/letter", views.qsic_letter, name="qsic_letter"),
 ]
